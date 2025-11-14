6bd2ed052bfce895a625bef7c8b79e3e0a3b7ccb
zadescoxp <zadescoxp@gmail.com>
2025-11-14 23:30:38 +0530

    a small change

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.json({ server: 'Server is running just fine' });
+    res.json({ response: 'Server is running just fine' });
 });
 
 export default app;
