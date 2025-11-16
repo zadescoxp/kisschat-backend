774ae5ec54b991213531406c71969bd4620b0a25
zadescoxp <zadescoxp@gmail.com>
2025-11-16 13:22:59 +0530

    email service added

---
+++ src/services/auth/email.services.ts
@@ -0,0 +1,37 @@
+import supabase from "../../config/supabase.config";
+
+export async function signUpWithEmail({ email, password }: { email: string; password: string }) {
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+    })
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return data;
+}
+
+export async function loginWithEmail({ email, password }: { email: string; password: string }) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return data;
+}
+
+export async function logout() {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return;
+}
