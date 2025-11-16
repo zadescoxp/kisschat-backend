f7802206d57cb1b7220c3ff876a610348b959fe6
zadescoxp <zadescoxp@gmail.com>
2025-11-16 10:36:08 +0530

    removed testing route entirely

--- src/middlewares/test.middlewares.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-
-export const testMiddleware = (req: Request, res: Response, next: Function) => {
-    console.log('Test middleware executed');
-    next();
-}
