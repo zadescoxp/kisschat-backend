d67c10ecd6c1c76537526cc9dd52e40979832648
zadescoxp <zadescoxp@gmail.com>
2025-11-16 13:21:39 +0530

    testing protected rate

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from "./app";
 import dotenv from "dotenv";
 import { Request, Response } from "express";
+import authRouter from "./routes/auth.routes";
+import { verifyAuthMiddleware } from "./middlewares/verifyAuth.middlewares";
 
 dotenv.config();
 
@@ -9,6 +11,16 @@ const PORT = process.env.PORT;
 app.get('/', (req: Request, res: Response) => {
     res.json({ response: 'Server health is ok !' });
 });
+
+// Example of a protected route
+app.get('/protected', verifyAuthMiddleware, (req: Request, res: Response) => {
+    res.json({
+        message: 'This is a protected route',
+        user: req.user
+    });
+});
+
+app.use('/auth', authRouter);
 
 try {
     app.listen(PORT, () => {
