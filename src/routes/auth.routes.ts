68c3f84300ac6e5b514fae5f5d3fa618fbaf03a0
zadescoxp <zadescoxp@gmail.com>
2025-11-16 13:22:22 +0530

    auth routes added

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { signUpController, loginController, logoutController } from "../controllers/auth.controllers";
+
+const authRouter = Router();
+
+authRouter.post('/signup', signUpController);
+authRouter.post('/login', loginController);
+authRouter.post('/logout', logoutController);
+
+export default authRouter;
