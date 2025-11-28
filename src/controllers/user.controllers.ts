bf440a002ddcd4847739bfdff1197e63ecfa6f2f
zadescoxp <zadescoxp@gmail.com>
2025-11-28 20:23:06 +0530

    create controller removed

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -2,32 +2,6 @@ import { Request, Response } from "express";
 import supabase from "../config/supabase.config";
 import { checkUser } from "../utils/check.util";
 
-export async function createUserController(req: Request, res: Response) {
-    const { username, email, avatar_url, last_login } = req.body;
-
-    const existingUser = await checkUser(email);
-
-    if (existingUser) {
-        return res.status(400).json({ error: "User with this email already exists" });
-    }
-
-    if (!username || !email) {
-        return res.status(400).json({ error: "Username and email are required" });
-    }
-    const { error } = await supabase.from('users').insert({
-        username,
-        email,
-        avatar_url,
-        status: "active",
-        last_login
-    })
-    if (error) {
-        return res.status(500).json({ error: error.message });
-    }
-
-    res.status(201).json({ message: "Account created successfully !" });
-}
-
 export async function updateUserController(req: Request, res: Response) {
     const { id } = req.params;
     const { username, avatar_url, status, last_login } = req.body;
