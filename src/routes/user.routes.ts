606a151836a2d3c8d5817451c6714ce9fc42d9d4
zadescoxp <zadescoxp@gmail.com>
2025-11-28 20:16:58 +0530

    removed creating of new users manually

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares";
-import { createUserController, deleteUserController, updateUserController } from "../controllers/user.controllers";
+import { deleteUserController, updateUserController } from "../controllers/user.controllers";
 
 const userRouter = Router();
 
-userRouter.post('/create', verifyAuthMiddleware, createUserController);
 userRouter.put('/update/:id', verifyAuthMiddleware, updateUserController);
 userRouter.delete('/delete/:id', verifyAuthMiddleware, deleteUserController);
 
