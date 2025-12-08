2f6211860f1422821b3dc7ea30384f4160a1af00
zadescoxp <zadescoxp@gmail.com>
2025-12-08 21:04:52 +0530

    chat controller for new character generation and repsonse generation added

--- src/controllers/chat.controllers.ts
+++ src/controllers/chat.controllers.ts
@@ -1,3 +1,19 @@
-export async function chatController() {
-    return;
+import { Request, Response } from "express";
+import { getCharacterResponse, getNewChatID } from "../services/chat_models/chat.services";
+
+export async function chatController(req: Request, res: Response) {
+    const { chat_id, prompt } = req.body;
+
+    const characterDetails = await getCharacterResponse(chat_id, prompt);
+
+    res.json({ characterDetails });
 }
+
+export async function newChatController(req: Request, res: Response) {
+    const { character_id } = req.body;
+
+    const user_id = req.user?.id;
+    const newChat = await getNewChatID(user_id || '', character_id);
+
+    res.json({ newChatID: newChat.chat_id });
+}   
