9c9c5d1b2aaf467f89861531ba8b431e335464ed
zadescoxp <zadescoxp@gmail.com>
2025-12-08 21:04:30 +0530

    model confguration added

--- src/config/openrouter.config.ts
+++ src/config/openrouter.config.ts
@@ -1,17 +1,19 @@
 import { OpenRouter } from "@openrouter/sdk";
 
-const client = new OpenRouter({
+const model = new OpenRouter({
     apiKey: process.env.OPENROUTER_API_KEY || "",
 })
 
-const getModelResponse = async (prompt: string) => {
-    const response = await client.chat.send({
-        model: "",
-        messages: [{
-            "role": "user",
-            "content": prompt
-        }]
-    })
+const getResponse = async (messages: any[]) => {
+    const response = await model.chat.send({
+        model: "mistralai/mixtral-8x7b-instruct",
+        messages: messages,
+        maxTokens: 1024,
+        temperature: 0.7,
+
+    });
+
+    return response;
 }
 
-export default getModelResponse;
+export default getResponse;
