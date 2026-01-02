50d423c7b4da47b8e2b3e41a1a19988f1bcb1470
zadescoxp <zadescoxp@gmail.com>
2026-01-02 20:21:03 +0530

    general testing route for experimentation purpose

---
+++ src/routes/test.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+
+const testRouter = Router();
+
+testRouter.post("/echo", (req, res) => {
+    const message = req.body.message;
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+    res.flushHeaders();
+
+    let counter = 0;
+
+    // if (!message) {
+    //     res.write("data: [ERROR] No message provided\n\n");
+    //     res.end();
+    //     return;
+    // }
+
+    const intervalId = setInterval(() => {
+        counter += 1;
+        res.write(`data: Echo ${counter}: ${message}\n\n`);
+        res.end();
+    }, 2000);
+
+    // req.on("close", () => {
+    //     clearInterval(intervalId);
+    //     res.end();
+    // });
+});
+
+export default testRouter;
