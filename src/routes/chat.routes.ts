42317f1ab0bacef4a2432174cc276cde3a1d3b75
zadescoxp <zadescoxp@gmail.com>
2026-01-06 14:46:05 +0530

    delete and get data routes added to chat endpoint

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { chatController, newChatController } from "../controllers/chat.controllers.js";
+import { chatController, deleteChatController, getChatByUserIdController, newChatController } from "../controllers/chat.controllers.js";
 
 const chatRouter = Router();
 
 chatRouter.post('/response', verifyAuthMiddleware, chatController);
 chatRouter.post('/new', verifyAuthMiddleware, newChatController);
+chatRouter.delete('/delete', verifyAuthMiddleware, deleteChatController);
+chatRouter.get('/get', verifyAuthMiddleware, getChatByUserIdController);
 
 export default chatRouter;
