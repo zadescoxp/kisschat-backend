bb6b31a30a23d30b499b96741f5f2959a343cab2
zadescoxp <zadescoxp@gmail.com>
2026-01-06 14:46:32 +0530

    added feature for visibility of chat

--- src/services/chat_models/chat.services.ts
+++ src/services/chat_models/chat.services.ts
@@ -1,5 +1,4 @@
 import supabase from "../../config/supabase.config.js";
-import getResponse from "../../config/openrouter.config.js";
 import { messageQueue } from "../../utils/queue.util.js";
 import { addSSEConnection } from "../../utils/sse.util.js";
 import { Response } from "express";
@@ -25,36 +24,11 @@ export const getCharacterResponse = async (chat_id: string, prompt: string, res:
 
     // Establish SSE connection and keep it open
     addSSEConnection(job.id!, res);
-    
+
     // Send initial status
     res.write(`data: ${JSON.stringify({ status: 'processing', jobId: job.id })}
 
 `);
-
-    // const response = await getResponse(chat_data.data.chats.concat([{ role: 'user', content: prompt }]));
-
-    // if (!response.choices || response.choices.length === 0) {
-    //     throw new Error('No response from model');
-    // }
-
-    // console.log(response);
-
-    // const updatedChats = [
-    //     ...chat_data.data.chats,
-    //     { role: 'user', content: prompt },
-    //     { role: 'assistant', content: response.choices[0].message.content }
-    // ];
-
-    // const { error } = await supabase.from('chats').update({ chats: updatedChats }).eq('chat_id', chat_id);
-
-    // if (error) {
-    //     throw new Error('Failed to update chat history');
-    // }
-
-    // return {
-    //     response: response.choices[0].message.content,
-    //     chatHistory: updatedChats
-    // };
 }
 
 const getCharacterDetails = async (character_id: string) => {
@@ -66,11 +40,17 @@ const getCharacterDetails = async (character_id: string) => {
     return data;
 }
 
-export const getNewChatID = async (user_id: string, character_id: string) => {
+export const getNewChatID = async (user_id: string, character_id: string, visibility: string) => {
     const characterDetails = await getCharacterDetails(character_id);
+
+    if (visibility !== 'public' && visibility !== 'private' && visibility !== 'unlisted' && visibility !== 'anonymous') {
+        throw new Error('Invalid visibility option');
+    }
+
     const { data, error } = await supabase.from('chats').insert({
         user_id,
         character_id,
+        visibility,
         chats: [{
             role: 'system',
             content: JSON.stringify(characterDetails)
