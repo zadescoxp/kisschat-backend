e1f154c160b8e98071d5807e5b048f32e5549c4a
zadescoxp <zadescoxp@gmail.com>
2026-01-06 14:45:44 +0530

    comment and get routes added to character endpoint

--- src/routes/characters.routes.ts
+++ src/routes/characters.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { createCharacterController, getCharacterByIdController, operationCharacterController } from "../controllers/character.controllers.js";
+import { createCharacterController, getCharacterByIdController, getCharacterByUserIdController, operationCharacterController } from "../controllers/character.controllers.js";
 
 const charactersRouter = Router();
 
 charactersRouter.post('/create', verifyAuthMiddleware, createCharacterController);
 charactersRouter.get("/get/:id", verifyAuthMiddleware, getCharacterByIdController);
 charactersRouter.post("/operation", verifyAuthMiddleware, operationCharacterController);
+charactersRouter.post("/comment", verifyAuthMiddleware, operationCharacterController);
+charactersRouter.get("/get", verifyAuthMiddleware, getCharacterByUserIdController);
 
 export default charactersRouter;
