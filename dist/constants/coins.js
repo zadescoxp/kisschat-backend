52e961e12056ec57ebb79d987bbaf3ab1c310701
zadescoxp <zadescoxp@gmail.com>
2026-01-11 01:17:13 +0530

    constants added for kiss coin deduction

---
+++ dist/constants/coins.js
@@ -0,0 +1,3 @@
+const create_character_coins = 50;
+const chat_character_coins = 10;
+export { create_character_coins, chat_character_coins };
---
+++ src/constants/coins.ts
@@ -0,0 +1,4 @@
+const create_character_coins = 50;
+const chat_character_coins = 10;
+
+export { create_character_coins, chat_character_coins };
