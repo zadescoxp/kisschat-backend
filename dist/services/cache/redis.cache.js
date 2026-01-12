c1612567a7a2ce8c8d3935534ed69bc34c481957
zadescoxp <zadescoxp@gmail.com>
2026-01-13 00:52:11 +0530

    redis caching funcitons init

---
+++ dist/services/cache/redis.cache.js
@@ -0,0 +1,16 @@
+import redis from "../../config/upstash.config.js";
+export async function checkCache(key) {
+    const value = await redis.get(key);
+    return value;
+}
+export async function setCache(key, value, expirationInSeconds) {
+    if (expirationInSeconds) {
+        await redis.set(key, value, { ex: expirationInSeconds });
+    }
+    else {
+        await redis.set(key, value);
+    }
+}
+export async function deleteCache(key) {
+    await redis.del(key);
+}
---
+++ src/services/cache/redis.cache.ts
@@ -0,0 +1,18 @@
+import redis from "../../config/upstash.config.js";
+
+export async function checkCache(key: string) {
+    const value = await redis.get(key);
+    return value;
+}
+
+export async function setCache(key: string, value: string, expirationInSeconds?: number) {
+    if (expirationInSeconds) {
+        await redis.set(key, value, { ex: expirationInSeconds });
+    } else {
+        await redis.set(key, value);
+    }
+}
+
+export async function deleteCache(key: string) {
+    await redis.del(key);
+}
