15ba7d1d2806825646ccedd3d974d69c2d7d2c37
zadescoxp <zadescoxp@gmail.com>
2026-01-14 00:37:54 +0530

    changed redirection url

--- dist/services/auth/x.services.js
+++ dist/services/auth/x.services.js
@@ -3,7 +3,7 @@ export async function signInWithX() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'twitter',
         options: {
-            redirectTo: 'https://kisschat-ai.vercel.app/api/auth/callback'
+            redirectTo: 'https://kisschat-ai.vercel.app/auth/callback'
         }
     });
     if (error) {
--- src/services/auth/x.services.ts
+++ src/services/auth/x.services.ts
@@ -4,7 +4,7 @@ export async function signInWithX() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'twitter',
         options: {
-            redirectTo: 'https://kisschat-ai.vercel.app/api/auth/callback'
+            redirectTo: 'https://kisschat-ai.vercel.app/auth/callback'
         }
     });
 
