46e762d1dca2b47eaed74b287b1cb6516318dd14
zadescoxp <zadescoxp@gmail.com>
2026-01-14 00:30:48 +0530

    redirect url changed

--- dist/services/auth/google.services.js
+++ dist/services/auth/google.services.js
@@ -3,7 +3,7 @@ export async function signInWithGoogle() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            redirectTo: 'https://kisschat-ai.vercel.app/auth/callback'
+            redirectTo: 'https://kisschat-ai.vercel.app/api/auth/callback'
         }
     });
     if (error) {
