462a27807bfbd19e13924901fc67fe011538c960
zadescoxp <zadescoxp@gmail.com>
2026-01-14 00:57:55 +0530

    x provider updated

--- dist/services/auth/x.services.js
+++ dist/services/auth/x.services.js
@@ -1,7 +1,7 @@
 import supabase from "../../config/supabase.config.js";
 export async function signInWithX() {
     const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: 'twitter',
+        provider: 'x',
         options: {
             redirectTo: 'https://kisschat-ai.vercel.app/auth/callback'
         }
