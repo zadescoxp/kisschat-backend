1ea5b9fde8466420559a068c11e2fd5b6ea08662
zadescoxp <zadescoxp@gmail.com>
2026-01-22 12:04:18 +0530

    rate and generate route added

---
+++ dist/routes/image.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
+import { generateImageController, rateImageController } from "../controllers/image.controllers.js";
+const imageRouter = Router();
+imageRouter.post('/rate', verifyAuthMiddleware, rateImageController);
+imageRouter.post('/generate', verifyAuthMiddleware, generateImageController);
+export default imageRouter;
