35fb63b818ec63957fee329b4ed9e1e17cc728ac
zadescoxp <zadescoxp@gmail.com>
2026-01-22 12:03:03 +0530

    image based coins added

--- src/constants/coins.ts
+++ src/constants/coins.ts
@@ -1,4 +1,9 @@
 const create_character_coins = 50;
 const chat_character_coins = 10;
+const image_base_coins = 5;
+const ultra_image_coins = 2;
+const max_image_coins = 4;
+const max_details_coins = 4;
+const max_creativity_coins = 4;
 
-export { create_character_coins, chat_character_coins };
+export { create_character_coins, chat_character_coins, image_base_coins, ultra_image_coins, max_image_coins, max_details_coins, max_creativity_coins };
