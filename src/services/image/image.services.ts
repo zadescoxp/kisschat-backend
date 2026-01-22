442ae3027722c215e32085726a1b439040328bef
zadescoxp <zadescoxp@gmail.com>
2026-01-22 12:04:29 +0530

    service added for image generation

---
+++ src/services/image/image.services.ts
@@ -0,0 +1,25 @@
+const image_api_url = process.env.IMAGE_GEN_API_URL;
+const promptchan_api_key = process.env.PROMPTCHAN_API_KEY;
+
+export const getImageApiUrl = async (details: object) => {
+    if (!image_api_url) {
+        throw new Error("IMAGE_GEN_API_URL is not defined in environment variables");
+    }
+
+    const res = await fetch(image_api_url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": promptchan_api_key || '',
+        },
+        body: JSON.stringify({
+            ...details
+        })
+    });
+
+    if (!res.ok) {
+        throw new Error(`Failed to fetch image API URL: ${res.statusText}`);
+    }
+    const data = await res.json();
+    return data.image;
+}
