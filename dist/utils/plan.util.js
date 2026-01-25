ecd63229969197c01ad224f3c6ce3a778374c587
zadescoxp <zadescoxp@gmail.com>
2026-01-25 18:50:15 +0530

    plan amount selection util function added

---
+++ dist/utils/plan.util.js
@@ -0,0 +1,49 @@
+import { basic_1, basic_12, basic_6, deluxe_12, deluxe_6, deluxe_1, pro_12, pro_6, pro_1 } from "../constants/premium.js";
+export default function planAmount(plan, duration) {
+    switch (plan) {
+        case 'basic':
+            if (duration == 1) {
+                return basic_1;
+            }
+            else if (duration == 6) {
+                return basic_6;
+            }
+            else if (duration == 12) {
+                return basic_12;
+            }
+            else {
+                return 0;
+            }
+            break;
+        case 'pro':
+            if (duration == 1) {
+                return pro_1;
+            }
+            else if (duration == 6) {
+                return pro_6;
+            }
+            else if (duration == 12) {
+                return pro_12;
+            }
+            else {
+                return 0;
+            }
+            break;
+        case 'deluxe':
+            if (duration == 1) {
+                return deluxe_1;
+            }
+            else if (duration == 6) {
+                return deluxe_6;
+            }
+            else if (duration == 12) {
+                return deluxe_12;
+            }
+            else {
+                return 0;
+            }
+            break;
+        default:
+            return 0;
+    }
+}
