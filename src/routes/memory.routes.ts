8e251b0e47030efbb9bfda5bffdd8f4ad128efb1
zadescoxp <zadescoxp@gmail.com>
2026-01-25 18:49:47 +0530

    memory fetching and saving routes added

---
+++ src/routes/memory.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
+import { getMemoryController, saveMemoryController } from "../controllers/memory.controllers.js";
+
+const memoryRouter = Router();
+
+memoryRouter.get("/get/:id", verifyAuthMiddleware, getMemoryController)
+memoryRouter.post("/save", verifyAuthMiddleware, saveMemoryController);
+
+export { memoryRouter };
