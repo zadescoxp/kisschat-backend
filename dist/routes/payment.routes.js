8e56a372b6e0ec01a5bfeedba38443d0458bb730
zadescoxp <zadescoxp@gmail.com>
2026-01-29 00:55:59 +0530

    specific route for crypto payments added

--- dist/routes/payment.routes.js
+++ dist/routes/payment.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { handlePaymentCallbackController, initiatePaymentController } from "../controllers/payment.controllers.js";
+import { handlePaymentCallbackController, initiateCryptoPaymentController } from "../controllers/payment.controllers.js";
 const paymentRouter = Router();
 paymentRouter.post("/callback", verifyAuthMiddleware, handlePaymentCallbackController);
-paymentRouter.post("/generate", verifyAuthMiddleware, initiatePaymentController);
+paymentRouter.post("/crypto/generate", verifyAuthMiddleware, initiateCryptoPaymentController);
 export default paymentRouter;
--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { handlePaymentCallbackController, initiatePaymentController } from "../controllers/payment.controllers.js";
+import { handlePaymentCallbackController, initiateCryptoPaymentController } from "../controllers/payment.controllers.js";
 
 const paymentRouter = Router();
 
 paymentRouter.post("/callback", verifyAuthMiddleware, handlePaymentCallbackController);
-paymentRouter.post("/generate", verifyAuthMiddleware, initiatePaymentController);
+paymentRouter.post("/crypto/generate", verifyAuthMiddleware, initiateCryptoPaymentController);
 
 export default paymentRouter;
