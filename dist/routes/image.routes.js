544f2073b22fb4dcc20011a59be8df0c77f705de
zadescoxp <zadescoxp@gmail.com>
2026-01-29 00:55:36 +0530

    save route added to payment

--- dist/routes/image.routes.js
+++ dist/routes/image.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { generateImageController, rateImageController } from "../controllers/image.controllers.js";
+import { generateImageController, rateImageController, saveGeneratedImage } from "../controllers/image.controllers.js";
 const imageRouter = Router();
 imageRouter.post('/rate', verifyAuthMiddleware, rateImageController);
 imageRouter.post('/generate', verifyAuthMiddleware, generateImageController);
+imageRouter.post('/save', verifyAuthMiddleware, saveGeneratedImage);
 export default imageRouter;
