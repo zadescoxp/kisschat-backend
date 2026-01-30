a5c3f1a3ef9bb5c5c8c6a8e8720973d81d8d17cd
zadescoxp <zadescoxp@gmail.com>
2026-01-30 22:42:54 +0530

    routes changed for the crypto callback webhook

--- dist/routes/payment.routes.js
+++ dist/routes/payment.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { handlePaymentCallbackController, initiateCryptoPaymentController } from "../controllers/payment.controllers.js";
+import { handleCryptoPaymentCallbackController, initiateCryptoPaymentController } from "../controllers/payment.controllers.js";
 const paymentRouter = Router();
-paymentRouter.post("/callback", verifyAuthMiddleware, handlePaymentCallbackController);
+paymentRouter.post("/crypto/webhook", handleCryptoPaymentCallbackController);
 paymentRouter.post("/crypto/generate", verifyAuthMiddleware, initiateCryptoPaymentController);
 export default paymentRouter;
