ee405256d1bec02072d5a7382c2ce20101ac2afe
zadescoxp <zadescoxp@gmail.com>
2026-02-02 05:07:50 +0530

    global utility function added for getting details for any user

---
+++ dist/utils/user.util.js
@@ -0,0 +1,12 @@
+import supabase from "../config/supabase.config.js";
+export async function getUserInfo(user_id) {
+    const { data, error } = await supabase
+        .from('profiles')
+        .select('*')
+        .eq('user_id', user_id)
+        .single();
+    if (error) {
+        throw new Error(error.message);
+    }
+    return data;
+}
---
+++ src/utils/user.util.ts
@@ -0,0 +1,15 @@
+import supabase from "../config/supabase.config.js";
+
+export async function getUserInfo(user_id: string) {
+    const { data, error } = await supabase
+        .from('profiles')
+        .select('*')
+        .eq('user_id', user_id)
+        .single();
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return data;
+}
