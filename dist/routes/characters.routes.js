7f4baa186d8f4abb3ff781a6efd8c5e5462b613b
zadescoxp <zadescoxp@gmail.com>
2026-02-02 03:01:32 +0530

    added middleware to the create character for username access

--- dist/routes/characters.routes.js
+++ dist/routes/characters.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { commentCharacterController, createCharacterController, getAllCharactersController, getCharacterByIdController, getCharacterByUserIdController, getCommentsByCharacterIdController, operationCharacterController } from "../controllers/character.controllers.js";
+import { commentCharacterController, commentInteractionController, createCharacterController, getAllCharactersController, getCharacterByIdController, getCharacterByUserIdController, getCommentsByCharacterIdController, operationCharacterController } from "../controllers/character.controllers.js";
 import { userMetaDataMiddleware } from "../middlewares/userMetaData.middleware.js";
 const charactersRouter = Router();
-charactersRouter.post('/create', verifyAuthMiddleware, createCharacterController);
+charactersRouter.post('/create', verifyAuthMiddleware, userMetaDataMiddleware, createCharacterController);
 charactersRouter.get("/get/:id", getCharacterByIdController);
 charactersRouter.get("/getAll", getAllCharactersController);
 charactersRouter.post("/operation", verifyAuthMiddleware, operationCharacterController);
 charactersRouter.post("/comment", verifyAuthMiddleware, userMetaDataMiddleware, commentCharacterController);
 charactersRouter.get("/getByUserId", verifyAuthMiddleware, userMetaDataMiddleware, getCharacterByUserIdController);
 charactersRouter.get("/comment/:id", verifyAuthMiddleware, getCommentsByCharacterIdController);
+charactersRouter.post("/comment/operation", verifyAuthMiddleware, commentInteractionController);
 export default charactersRouter;
