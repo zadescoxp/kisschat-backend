6e36f86a2fe5b2385f3ea22ca4838df3cecd453b
zadescoxp <zadescoxp@gmail.com>
2026-02-06 02:02:19 +0530

    social media update routes added

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { deleteUserController, followUserController, getUserByIdController, getUserPremiumByIdController, updateUserController } from "../controllers/user.controllers.js";
+import { deleteUserController, followUserController, getUserByIdController, getUserPremiumByIdController, updateUserController, updateUserSocialMediaController } from "../controllers/user.controllers.js";
 
 const userRouter = Router();
 
@@ -9,5 +9,6 @@ userRouter.delete('/delete/:id', verifyAuthMiddleware, deleteUserController);
 userRouter.post('/follow', verifyAuthMiddleware, followUserController);
 userRouter.get('/getByUserId/:id', getUserByIdController);
 userRouter.get('/getPremiumByUserId/:id', getUserPremiumByIdController);
+userRouter.put('/updateSocialMedia', verifyAuthMiddleware, updateUserSocialMediaController);
 
 export default userRouter;
