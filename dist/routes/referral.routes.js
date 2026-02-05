26550ed953fd5289cc8ae20fad9a39f6c68eee0f
zadescoxp <zadescoxp@gmail.com>
2026-02-06 01:35:20 +0530

    referral route init

---
+++ dist/routes/referral.routes.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { referralCodeController } from "../controllers/referral.controllers.js";
+import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
+const referralRouter = Router();
+referralRouter.post('/code', verifyAuthMiddleware, referralCodeController);
+export default referralRouter;
