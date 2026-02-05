9c9de52825ca05b0a03869967ccd77f24a625671
zadescoxp <zadescoxp@gmail.com>
2026-02-06 01:35:11 +0530

    referral controller added

---
+++ dist/controllers/referral.controllers.js
@@ -0,0 +1,16 @@
+import { handleReferral } from "../utils/referral.util.js";
+export async function referralCodeController(req, res) {
+    const { referral_code, code } = req.body;
+    const user_id = req.user?.id;
+    // if (!referral_code || !code) {
+    //     return res.status(400).json({ error: "Referral code and activity code are required." });
+    // }
+    try {
+        await handleReferral(referral_code, code, user_id || '');
+    }
+    catch (error) {
+        console.error("Referral code check error:", error);
+        return res.status(400).json({ error: error.message || "Failed to process referral code." });
+    }
+    return res.json({ message: "Referral code processed successfully." });
+}
---
+++ src/controllers/referral.controllers.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { handleReferral } from "../utils/referral.util.js";
+
+export async function referralCodeController(req: Request, res: Response) {
+    const { referral_code, code } = req.body;
+    const user_id = req.user?.id;
+
+    // if (!referral_code || !code) {
+    //     return res.status(400).json({ error: "Referral code and activity code are required." });
+    // }
+
+    try {
+        await handleReferral(referral_code, code, user_id || '');
+    }
+    catch (error: any) {
+        console.error("Referral code check error:", error);
+        return res.status(400).json({ error: error.message || "Failed to process referral code." });
+    }
+
+    return res.json({ message: "Referral code processed successfully." });
+}
