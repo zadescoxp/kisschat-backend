e8a75a835d294347d8ddd598ce9fdaf7ad75d4db
zadescoxp <zadescoxp@gmail.com>
2026-02-06 01:34:31 +0530

    referral activity and points constants

---
+++ dist/constants/referral.js
@@ -0,0 +1,19 @@
+const activity = [
+    {
+        activity: "Referral Code Activated",
+        points: 50
+    },
+    {
+        activity: "First Chat",
+        points: 20
+    },
+    {
+        activity: "First Image Generation",
+        points: 30
+    },
+    {
+        activity: "First Purchase",
+        points: 100
+    }
+];
+export { activity };
