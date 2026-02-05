c82920e1f9d00d7398bfcaf53ef78bb18a4a463f
zadescoxp <zadescoxp@gmail.com>
2026-02-06 03:42:42 +0530

    pfp update route added

--- dist/routes/user.routes.js
+++ dist/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { deleteUserController, followUserController, getUserByIdController, getUserPremiumByIdController, updateUserController, updateUserSocialMediaController } from "../controllers/user.controllers.js";
+import { uploadSingleImage } from "../middlewares/upload.middlewares.js";
+import { deleteUserController, followUserController, getUserByIdController, getUserPremiumByIdController, updateUserController, updateUserProfilePictureController, updateUserSocialMediaController } from "../controllers/user.controllers.js";
 const userRouter = Router();
 userRouter.put('/update', verifyAuthMiddleware, updateUserController);
 userRouter.delete('/delete/:id', verifyAuthMiddleware, deleteUserController);
@@ -8,4 +9,5 @@ userRouter.post('/follow', verifyAuthMiddleware, followUserController);
 userRouter.get('/getByUserId/:id', getUserByIdController);
 userRouter.get('/getPremiumByUserId/:id', getUserPremiumByIdController);
 userRouter.put('/updateSocialMedia', verifyAuthMiddleware, updateUserSocialMediaController);
+userRouter.put('/updateProfilePicture', verifyAuthMiddleware, uploadSingleImage, updateUserProfilePictureController);
 export default userRouter;
