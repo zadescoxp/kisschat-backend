d2c2156d41476312e5e58a70c8d3b532b408fa59
zadescoxp <zadescoxp@gmail.com>
2026-02-06 02:02:09 +0530

    image retrieval routes added

--- dist/routes/image.routes.js
+++ dist/routes/image.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { generateImageController, rateImageController, saveGeneratedImage } from "../controllers/image.controllers.js";
+import { generateImageController, getImageByIdController, getImageByUserIdController, rateImageController, saveGeneratedImage } from "../controllers/image.controllers.js";
 const imageRouter = Router();
 imageRouter.post('/rate', verifyAuthMiddleware, rateImageController);
 imageRouter.post('/generate', verifyAuthMiddleware, generateImageController);
 imageRouter.post('/save', verifyAuthMiddleware, saveGeneratedImage);
+imageRouter.get('/getImageByUserId', verifyAuthMiddleware, getImageByUserIdController);
+imageRouter.get('/getImageById', verifyAuthMiddleware, getImageByIdController);
 export default imageRouter;
