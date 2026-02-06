56d5cf11ad0706aa2e3c4b6aa958a29c13cb152c
zadescoxp <zadescoxp@gmail.com>
2026-02-06 21:01:21 +0530

    user meta data middleware added

--- dist/routes/image.routes.js
+++ dist/routes/image.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
 import { generateImageController, getImageByIdController, getImageByUserIdController, likeImageController, rateImageController, saveGeneratedImage } from "../controllers/image.controllers.js";
+import { userMetaDataMiddleware } from "../middlewares/userMetaData.middleware.js";
 const imageRouter = Router();
 imageRouter.post('/rate', verifyAuthMiddleware, rateImageController);
-imageRouter.post('/generate', verifyAuthMiddleware, generateImageController);
-imageRouter.post('/save', verifyAuthMiddleware, saveGeneratedImage);
+imageRouter.post('/generate', verifyAuthMiddleware, userMetaDataMiddleware, generateImageController);
+imageRouter.post('/save', verifyAuthMiddleware, userMetaDataMiddleware, saveGeneratedImage);
 imageRouter.post('/getImageByUserId', verifyAuthMiddleware, getImageByUserIdController);
 imageRouter.post('/getImageById', verifyAuthMiddleware, getImageByIdController);
 imageRouter.post('/like', verifyAuthMiddleware, likeImageController);
