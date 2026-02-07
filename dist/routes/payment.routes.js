80bb23b6dbeb2610d4d1e49e34010eb9b33e618a
zadescoxp <zadescoxp@gmail.com>
2026-02-07 09:11:19 +0530

    added kisscoins purchase route

--- dist/routes/payment.routes.js
+++ dist/routes/payment.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { handleCryptoPaymentCallbackController, initiateCryptoPaymentController } from "../controllers/payment.controllers.js";
+import { handleCryptoPaymentCallbackController, handleKissCoinsCryptoPaymentCallbackController, initiateCryptoPaymentController } from "../controllers/cryptoPayment.controllers.js";
 const paymentRouter = Router();
 paymentRouter.post("/crypto/webhook", handleCryptoPaymentCallbackController);
 paymentRouter.post("/crypto/generate", verifyAuthMiddleware, initiateCryptoPaymentController);
+paymentRouter.post("/crypto/kiss-coins/webhook", handleKissCoinsCryptoPaymentCallbackController);
+paymentRouter.post("/crypto/kiss-coins/generate", verifyAuthMiddleware, initiateCryptoPaymentController);
 export default paymentRouter;
