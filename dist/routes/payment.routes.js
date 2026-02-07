73cd8fd1355bf1cdca64baf493e3d8344310258f
zadescoxp <zadescoxp@gmail.com>
2026-02-07 09:44:06 +0530

    controller changed

--- dist/routes/payment.routes.js
+++ dist/routes/payment.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { handleCryptoPaymentCallbackController, handleKissCoinsCryptoPaymentCallbackController, initiateCryptoPaymentController } from "../controllers/cryptoPayment.controllers.js";
+import { handleCryptoPaymentCallbackController, handleKissCoinsCryptoPaymentCallbackController, initiateCryptoPaymentController, initiateKissCoinsCryptoPaymentController } from "../controllers/cryptoPayment.controllers.js";
 const paymentRouter = Router();
 paymentRouter.post("/crypto/webhook", handleCryptoPaymentCallbackController);
 paymentRouter.post("/crypto/generate", verifyAuthMiddleware, initiateCryptoPaymentController);
 paymentRouter.post("/crypto/kiss-coins/webhook", handleKissCoinsCryptoPaymentCallbackController);
-paymentRouter.post("/crypto/kiss-coins/generate", verifyAuthMiddleware, initiateCryptoPaymentController);
+paymentRouter.post("/crypto/kiss-coins/generate", verifyAuthMiddleware, initiateKissCoinsCryptoPaymentController);
 export default paymentRouter;
