48a26151d85155b8d6a675326d76d169a1f87c28
zadescoxp <zadescoxp@gmail.com>
2026-02-09 00:47:23 +0530

    routes made for crud

--- src/routes/memory.routes.ts
+++ src/routes/memory.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
-import { getMemoryController, saveMemoryController } from "../controllers/memory.controllers.js";
+import { getMemoryController, saveMemoryController, getPublicMemoriesController, deleteMemoryController, updateMemoryController } from "../controllers/memory.controllers.js";
 
 const memoryRouter = Router();
 
 memoryRouter.get("/get/:id", verifyAuthMiddleware, getMemoryController)
 memoryRouter.post("/save", verifyAuthMiddleware, saveMemoryController);
+memoryRouter.get("/public", getPublicMemoriesController);
+memoryRouter.delete("/delete", verifyAuthMiddleware, deleteMemoryController);
+memoryRouter.put("/update", verifyAuthMiddleware, updateMemoryController);
 
 export { memoryRouter };
