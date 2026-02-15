ff1907184f972dae901a6f4a334932f3da4235e8
zadescoxp <zadescoxp@gmail.com>
2026-02-15 11:34:45 +0530

    automation for characters and images routes created

---
+++ dist/routes/automate.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { automateController, enhanceImagePromptController } from "../controllers/automate.controllers";
+import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares";
+const automateRouter = Router();
+automateRouter.post("/automate", verifyAuthMiddleware, automateController);
+automateRouter.post("/enhance-image-prompt", verifyAuthMiddleware, enhanceImagePromptController);
+export default automateRouter;
