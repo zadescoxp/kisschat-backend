ac5a099589bee3dfb5cd4f06ce21b7b526827fca
zadescoxp <zadescoxp@gmail.com>
2026-02-18 22:33:26 +0530

    rewards routes made

---
+++ src/routes/rewards.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { checkRewardClaim, getRewards } from "../controllers/rewards.controllers.js";
+import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middlewares.js";
+
+const rewardsRouter = Router();
+
+rewardsRouter.get("/check-claim", verifyAuthMiddleware, checkRewardClaim);
+rewardsRouter.post("/get-rewards", verifyAuthMiddleware, getRewards);
+
+export default rewardsRouter;
