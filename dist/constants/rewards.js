14a24316950ec5171c1e3fac68e14addfdccbc5f
zadescoxp <zadescoxp@gmail.com>
2026-02-18 22:36:03 +0530

    rewards contants added

---
+++ dist/constants/rewards.js
@@ -0,0 +1,31 @@
+const rewardsAmount = [
+    {
+        day: 1,
+        amount: 50
+    },
+    {
+        day: 2,
+        amount: 100
+    },
+    {
+        day: 3,
+        amount: 150
+    },
+    {
+        day: 4,
+        amount: 200
+    },
+    {
+        day: 5,
+        amount: 250
+    },
+    {
+        day: 6,
+        amount: 300
+    },
+    {
+        day: 7,
+        amount: 350
+    }
+];
+export { rewardsAmount };
